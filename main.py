cf2eaa2a0d46514ca14cea0cc298d4b49a33bc02
Niva! <nivthehpfan@outlook.com>
2026-07-02 11:07:33 +0100

    A B C D E F G H I_WILL_HIT_YOU_WITH_A_FISH J K L M N O P

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import f, lps, psn, psw
 if f.test() and lps.test() and psn.test() and psw.test(): print('All scripts loaded.')
 else: print('An error ocurred.'), exit()
 
-print('Please do not use this toolkit in a way which harasses the 1990 Computer Misuse Act.\nThis toolkit only has over-simple/basic tools and their are also lots of bugs, as this is the developer\'s first time writing full code, sorry.') 
+print('Please do not use this toolkit in a way which harasses the 1990 Computer Misuse Act.\nThis toolkit only has over-simple/basic tools and there are also lots of bugs, as this is the developer\'s first time writing full code, sorry.') 
 userscmd = str(input('chaos>>> '))
 
 try: f.process_cmd(userscmd) 

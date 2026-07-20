70865ddb0114ea640d7a3d1643b04b76270cdfed
Niv! <nivthehpfan@outlook.com>
2026-07-20 09:10:23 +0100

    upd readability

--- psw.py
+++ psw.py
@@ -3,17 +3,17 @@ def test():
 
 import nmap
 
-def PiNgSwEePeR(SUBNET):
+def psw(SUBNET):
     nm = nmap.PortScanner()
     print(f'Please wait. Pinging devices on subnet {SUBNET}.')
     output = []
     try:
         result = nm.scan(hosts=SUBNET, arguments='-sn')
         for host in nm.all_hosts():
-            stateofTHING = nm[host].state()
-            if stateofTHING == 'up':
+            State = nm[host].state()
+            if State == 'up':
                 output.append(f'The device with the IPv4 address {host} is on and connected to the internet.')
         for i in range(len(output)): print(output[i])
-    except BaseException as aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:
-        print(f'Error:{aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa}')
+    except BaseException as Exception:
+        print(f'Error:{Exception}')
 
